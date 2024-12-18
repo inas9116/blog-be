ad3a56d3e2fa3a08abef8e00bb26ab6dd5b71888
inas9116 <139898118+inas9116@users.noreply.github.com>
2024-12-18 18:13:13 +0700

    Initial commit

---
+++ src/routes/sample.router.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+import { getSamplesController} from "../controller/sample.controller"
+
+const router = Router();
+
+router.get("/", getSamplesController);
+
+export default router;
